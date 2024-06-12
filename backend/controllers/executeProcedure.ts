0b9ecfae83da67a139ebe700b679039ad722a21a
Victor Aymerich Quesada <vaymerich@estudiantec.cr>
2024-06-12 11:22:42 -0600

    Function getObject
    The function getObject is added which works like execute procedure but gives the object so it can be worked. This function is necessary for validations in the cruds

--- backend/controllers/executeProcedure.ts
+++ backend/controllers/executeProcedure.ts
@@ -20,3 +20,24 @@ export async function executeProcedure(
         res.status(500).send(`Database failed: ${errorMessage}`);
     }
 }
+
+//function the same as executeProcedure but returns the object
+export async function getObject(
+    res: Response, 
+    procedure: string, 
+    inputs: { name: string, type: any, value: any }[], 
+    successStatus: number, 
+    successMessage: string,
+    errorMessage: string
+) {
+    try {
+        const connection = await pool;
+        const request = connection.request();
+        inputs.forEach(input => request.input(input.name, input.type, input.value));
+        const result = await request.execute(procedure);
+        return result
+    } catch (err) {
+        console.error('Database operation failed:', err);
+        res.status(500).send(`Database failed: ${errorMessage}`);
+    }
+}
