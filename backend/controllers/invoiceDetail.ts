8b4e7c48f3f102df5e7b92d79e133f7cd81b4a15
Fabricio Solis <sfabricito@gmail.com>
2024-06-13 01:29:07 -0600

    Invoice Detail endpoint modified: Change stored procedures names

--- backend/controllers/invoiceDetail.ts
+++ backend/controllers/invoiceDetail.ts
@@ -8,22 +8,20 @@ async function CreateInvoiceDetail(req: Request, res: Response) {
         return res.status(400).send("Missing required fields");
     }
 
-    //validate IDInvoice
     const invoice = await getItem(res,
-        'SPReadInvoiceByID',
+        'ReadByIDInvoice',
         [{ name: 'IDInvoice', type: sql.Int, value: IDInvoice }]
     );
     if (invoice?.recordset.length == 0) { return res.status(404).send("Invoice Detail not created: Invoice not found"); }
 
-    //validate IDProduct
     const product = await getItem(res,
-        'GetProductById',
+        'ReadByIDProduct',
         [{ name: 'IDProduct', type: sql.Int, value: IDProduct }]
     );
     if (product?.recordset.length == 0) { return res.status(404).send("Invoice Detail not created: Product not found"); }
 
     await executeProcedure(res,
-        'SPCreateInvoiceDetail',
+        'CreateInvoiceDetail',
         [
             { name: 'IDInvoice', type: sql.Int, value: IDInvoice },
             { name: 'IDProduct', type: sql.Int, value: IDProduct },
@@ -38,7 +36,7 @@ async function CreateInvoiceDetail(req: Request, res: Response) {
 
 async function ReadAllInvoiceDetails(req: Request, res: Response) {
     await executeProcedure(res,
-        'SPReadAllInvoiceDetails',
+        'ReadAllInvoiceDetails',
         [],
         200,
         "Invoice details retrieved successfully",
@@ -48,7 +46,7 @@ async function ReadAllInvoiceDetails(req: Request, res: Response) {
 async function ReadInvoiceDetailById(req: Request, res: Response) {
     const IDInvoiceDetail = req.params.id;
     await executeProcedure(res,
-        'SPReadInvoiceDetailByID',
+        'ReadByIDInvoiceDetail',
         [{ name: 'IDInvoiceDetail', type: sql.Int, value: IDInvoiceDetail }],
         200,
         "Invoice detail retrieved successfully",
@@ -58,9 +56,8 @@ async function ReadInvoiceDetailById(req: Request, res: Response) {
 async function UpdateInvoiceDetail(req: Request, res: Response) {
     const IDInvoiceDetail = req.params.id;
 
-    //validate IDProduct
     const invoiceDetail = await getItem(res,
-            'SPReadInvoiceDetailByID',
+            'ReadByIDInvoiceDetail',
             [{ name: 'IDInvoiceDetail', type: sql.Int, value: IDInvoiceDetail }]
     );
     if (invoiceDetail?.recordset.length == 0) { return res.status(404).send("Invoice detail not found"); }
@@ -70,26 +67,21 @@ async function UpdateInvoiceDetail(req: Request, res: Response) {
     const Description = req.body.Description || invoiceDetail?.recordset[0].Description;
     const Quantity = req.body.Quantity || invoiceDetail?.recordset[0].Quantity;
     const Price = req.body.Price || invoiceDetail?.recordset[0].Price;
-
-    console.log(invoiceDetail?.recordset[0]);
     
-    
-    //validate IDInvoice
     const invoice = await getItem(res,
-        'SPReadInvoiceByID',
+        'ReadByIDInvoice',
         [{ name: 'IDInvoice', type: sql.Int, value: IDInvoice }]
     );
     if (invoice?.recordset.length == 0) { return res.status(404).send("Invoice Detail not updated: Invoice not found"); }
 
-    //validate IDProduct
     const product = await getItem(res,
-        'GetProductById',
+        'ReadByIDProduct',
         [{ name: 'IDProduct', type: sql.Int, value: IDProduct }]
     );
     if (product?.recordset.length == 0) { return res.status(404).send("Invoice Detail not updated: Product not found"); }
     
     await executeProcedure(res,
-        'SPUpdateInvoiceDetail',
+        'UpdateInvoiceDetail',
         [
             { name: 'IDInvoiceDetail', type: sql.Int, value: IDInvoiceDetail },
             { name: 'IDInvoice', type: sql.Int, value: IDInvoice },
@@ -106,15 +98,8 @@ async function UpdateInvoiceDetail(req: Request, res: Response) {
 async function DeleteInvoiceDetail(req: Request, res: Response) {
     const IDInvoiceDetail = req.params.id;
 
-    //validate IDProduct
-    const invoiceDetail = await getItem(res,
-        'SPReadInvoiceDetailByID',
-        [{ name: 'IDInvoiceDetail', type: sql.Int, value: IDInvoiceDetail }]
-    );
-    if (invoiceDetail?.recordset.length == 0) { return res.status(404).send("Invoice Detail not deleted: Invoice detail not found"); }
-
     await executeProcedure(res,
-        'SPDeleteInvoiceDetail',
+        'DeleteInvoiceDetail',
         [{ name: 'IDInvoiceDetail', type: sql.Int, value: IDInvoiceDetail }],
         200,
         "Invoice detail deleted successfully",
