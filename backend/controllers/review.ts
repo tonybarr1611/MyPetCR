b14b1f83a10ef528b53a9594bd6ed967e298b243
Victor Aymerich Quesada <vaymerich@estudiantec.cr>
2024-06-13 11:28:53 -0600

    Review endpoint modified: Stored procedures names and validations
    Change in the names of the stored procedures to make it work with the azure database, already tested, also some changes in validations so they work correctly

--- backend/controllers/review.ts
+++ backend/controllers/review.ts
@@ -4,7 +4,7 @@ import { executeProcedure, getObject } from './executeProcedure';
 
 async function AllReviews(req: Request, res: Response) {
     await executeProcedure(res,
-        'GetAllReviews',
+        'ReadAllReviews',
         [],
         200,
         "Reviews retrieved successfully",
@@ -14,7 +14,7 @@ async function AllReviews(req: Request, res: Response) {
 async function ReviewById(req: Request, res: Response) {
     const IDReview = req.params.id;
     await executeProcedure(res,
-        'GetReviewById',
+        'ReadByIDReview',
         [{ name: 'IDReview', type: sql.Int, value: IDReview }],
         200,
         "Review retrieved successfully",
@@ -26,6 +26,25 @@ async function CreateReview(req: Request, res: Response) {
     //validate the json
     if (!IDProduct || !IDClient || !Description || !Rating || !DateTime) {
         return res.status(400).send("Missing required fields");}
+
+    //validate idProduct
+    const product = await getObject(res,
+        'ReadByIDProduct',
+        [{ name: 'IDProduct', type: sql.Int, value: IDProduct }],
+        200,
+        "Product retrieved successfully",
+        "Product not retrieved");
+    if (!product || product.recordset.length === 0) { return res.status(404).send("Product not found");}
+
+    //validate idClient
+    const client = await getObject(res,
+        'ReadByIDClient',
+        [{ name: 'IDClient', type: sql.Int, value: IDClient }],
+        200,
+        "Client retrieved successfully",
+        "Client not retrieved");
+    if (!client || client.recordset.length === 0) { return res.status(404).send("Client not found");}
+
     await executeProcedure(res,
         'CreateReview',
         [{ name: 'IDProduct', type: sql.Int, value: IDProduct },
@@ -44,23 +63,42 @@ async function UpdateReview(req: Request, res: Response) {
     
     //search for the review
     const review = await getObject(res,
-        'GetReviewById',
+        'ReadByIDReview',
         [{ name: 'IDReview', type: sql.Int, value: IDReview }],
         200,
         "Review retrieved successfully",
         "Review not retrieved");
-    if (!review) {
-        return res.status(404).send("Not review found");}
+    if (!review || review.recordset.length === 0) {return res.status(404).send("Not review found");}
+
     //validate the json
     IDProduct = IDProduct || review.recordset[0].IDProduct;
     IDClient = IDClient || review.recordset[0].IDClient;
     Description = Description || review.recordset[0].Description;
     Rating = Rating || review.recordset[0].Rating;
     DateTime = DateTime || review.recordset[0].DateTime;
-    
-    if (Rating < 0 || Rating > 5) { return res.status(400).send("Rating must be between 0 and 5");}
-    //update the review
 
+    //validate idProduct
+    const product = await getObject(res,
+        'ReadByIDProduct',
+        [{ name: 'IDProduct', type: sql.Int, value: IDProduct }],
+        200,
+        "Product retrieved successfully",
+        "Product not retrieved");
+    if (!product || product.recordset.length === 0) { return res.status(404).send("Product not found");}
+
+    //validate idClient
+    const client = await getObject(res,
+        'ReadByIDClient',
+        [{ name: 'IDClient', type: sql.Int, value: IDClient }],
+        200,
+        "Client retrieved successfully",
+        "Client not retrieved");
+    if (!client || client.recordset.length === 0) { return res.status(404).send("Client not found");}
+    
+    //validate rating
+    if (Rating < 0 || Rating > 5) { return res.status(400).send("Rating must be between 0 and 5");}
+
+    //update the review
     await executeProcedure(res,
         'UpdateReview',
         [{ name: 'IDReview', type: sql.Int, value: IDReview },
