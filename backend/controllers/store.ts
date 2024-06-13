2002da735110edcc8f9caa6c9a8830a492682f67
Victor Aymerich Quesada <vaymerich@estudiantec.cr>
2024-06-12 18:16:07 -0600

    Store Stored procedures created

---
+++ backend/controllers/store.ts
@@ -0,0 +1,71 @@
+import sql from 'mssql';
+import { Request, Response } from 'express';
+import { executeProcedure, getObject } from './executeProcedure';
+
+async function AllStores(req: Request, res: Response) {
+    await executeProcedure(res,
+        'GetAllStores',
+        [],
+        200,
+        "Stores retrieved successfully",
+        "Stores not retrieved");
+}
+
+async function StoreById(req: Request, res: Response) {
+    const IDStore = req.params.id;
+    await executeProcedure(res,
+        'GetStoreById',
+        [{ name: 'IDStore', type: sql.Int, value: IDStore }],
+        200,
+        "Store retrieved successfully",
+        "Store not retrieved");
+};
+
+async function CreateStore(req: Request, res: Response) {
+    const {Location} = req.body;
+    //validate the json
+    if (!Location) 
+        { return res.status(400).send("Missing required fields");}
+    await executeProcedure(res,
+        'CreateStore',
+        [{ name: 'Location', type: sql.NVarChar(128), value: Location }],
+        201,
+        "Store created successfully",
+        "Store not created");
+
+};
+
+async function UpdateStore(req: Request, res: Response) {
+    const IDStore = req.params.id;
+    const {Location} = req.body;
+
+    //validate the json
+    if ( !Location) 
+        { return res.status(400).send("Missing required fields");}
+
+    await executeProcedure(res,
+        'UpdateStore',
+        [{ name: 'IDStore', type: sql.Int, value: IDStore },
+        { name: 'Location', type: sql.NVarChar(128), value: Location }],
+        200,
+        "Store updated successfully",
+        "Store not updated");
+};
+
+async function DeleteStore(req: Request, res: Response) {
+    const IDStore = req.params.id;
+    await executeProcedure(res,
+        'DeleteStore',
+        [{ name: 'IDStore', type: sql.Int, value: IDStore }],
+        200,
+        "Store deleted successfully",
+        "Store not deleted");
+}
+
+export default {
+    AllStores,
+    StoreById,
+    CreateStore,
+    UpdateStore,
+    DeleteStore
+};
