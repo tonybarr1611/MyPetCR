cc11e4886b1a03e40c4868be66494436146ea56d
Victor Aymerich Quesada <vaymerich@estudiantec.cr>
2024-06-13 10:59:18 -0600

    Log endpoint modified : Sotred procedures name changed
    Change in the names of the stored procedures to make it work with the azure database, already tested

--- backend/controllers/log.ts
+++ backend/controllers/log.ts
@@ -4,29 +4,49 @@ import { executeProcedure, getObject } from './executeProcedure';
 
 async function AllLogs(req: Request, res: Response) {
     await executeProcedure(res,
-        'GetAlllogs',
+        'ReadAlllogs',
         [],
         200,
-        "Product types retrieved successfully",
-        "Product types not retrieved");
+        "Log retrieved successfully",
+        "Log types not retrieved");
 }
 
 async function LogById(req: Request, res: Response) {
     const IDLog = req.params.id;
     await executeProcedure(res,
-        'GetLogById',
+        'ReadByIDLog',
         [{ name: 'IDLog', type: sql.Int, value: IDLog }],
         200,
-        "Product type retrieved successfully",
-        "Product type not retrieved");
+        "Log retrieved successfully",
+        "Log not retrieved");
 }
 
 async function CreateLog(req: Request, res: Response) {
-    const { IDLogType, IDUser, DateTime, Description } = req.body;
+    const { IDLogType, IDUser, DateTime, Description } = req.body; 
+
     //validate the json
     if (!IDLogType || !IDUser || !DateTime || !Description) {
         return res.status(400).send("Missing required fields");}
+    
+    //validate idLogType
+    const logType = await getObject(res,
+        'ReadByIDLogType',
+        [{ name: 'IDLogType', type: sql.Int, value: IDLogType }],
+        200,
+        "Log retrieved successfully",
+        "Log not retrieved");
+    if (!logType || logType.recordset.length === 0) { return res.status(404).send("Log type not found"); }
 
+    //validate idUser
+    const user = await getObject(res,
+        'ReadByIDUser',
+        [{ name: 'IDUser', type: sql.Int, value: IDUser }],
+        200,
+        "User retrieved successfully",
+        "User not retrieved");
+    if (!user || user.recordset.length === 0) { return res.status(404).send("User not found"); }
+
+    //Create the log
     await executeProcedure(res,
         'CreateLog',
         [{ name: 'IDLogType', type: sql.Int, value: IDLogType },
@@ -34,27 +54,47 @@ async function CreateLog(req: Request, res: Response) {
         { name: 'DateTime', type: sql.DateTime, value: DateTime },
         { name: 'Description', type: sql.NVarChar, value: Description }],
         201,
-        "Product type created successfully",
-        "Product type not created");
+        "Log created successfully",
+        "Log not created");
 };
 
 async function UpdateLog(req: Request, res: Response) {
     const IDLog = req.params.id;
     let { IDLogType, IDUser, DateTime, Description } = req.body;
+
     //search for the log to update
     const log = await getObject(res,
-        'GetLogById',
+        'ReadByIDLog',
         [{ name: 'IDLog', type: sql.Int, value: IDLog }],
         200,
         "Log retrieved successfully",
         "Log not retrieved");
-    if (!log) return res.status(404).send("Log not found");
+    if (!log || log.recordset.length === 0) return res.status(404).send("Log not found");
 
     //validate the json
     IDLogType = IDLogType || log.recordset[0].IDLogType;
     IDUser = IDUser || log.recordset[0].IDUser;
     DateTime = DateTime || log.recordset[0].DateTime;
     Description = Description || log.recordset[0].Description;
+
+    //validate idLogType
+    const logType = await getObject(res,
+        'ReadByIDLogType',
+        [{ name: 'IDLogType', type: sql.Int, value: IDLogType }],
+        200,
+        "Log type retrieved successfully",
+        "Log type not retrieved");
+
+    if (!logType || logType.recordset.length === 0) { return res.status(404).send("Log type not found"); }
+
+    //validate idUser
+    const user = await getObject(res,
+        'ReadByIDUser',
+        [{ name: 'IDUser', type: sql.Int, value: IDUser }],
+        200,
+        "User retrieved successfully",
+        "User not retrieved");
+    if (!user || user.recordset.length === 0) { return res.status(404).send("User not found"); }
 
     //update the log
     await executeProcedure(res,
@@ -65,8 +105,8 @@ async function UpdateLog(req: Request, res: Response) {
         { name: 'DateTime', type: sql.DateTime, value: DateTime },
         { name: 'Description', type: sql.NVarChar, value: Description }],
         200,
-        "Product type updated successfully",
-        "Product type not updated");
+        "Log updated successfully",
+        "Log not updated");
 };
 
 async function DeleteLog(req: Request, res: Response) {
@@ -75,8 +115,8 @@ async function DeleteLog(req: Request, res: Response) {
         'DeleteLog',
         [{ name: 'IDLog', type: sql.Int, value: IDLog }],
         200,
-        "Product type deleted successfully",
-        "Product type not deleted");
+        "Log deleted successfully",
+        "Log not deleted");
 }
 
 export default {
