fe472fe7396a517ffbdc99f7a9376bb0a2c87dc2
tony1611 <antbarrantes@estudiantec.cr>
2024-06-14 13:03:38 -0600

    MedicalFiles refactoring
    Refactored how the data is showed in the MedicalFiles table.

--- src/Dashboard/MedicalFiles/MedicalFiles.tsx
+++ src/Dashboard/MedicalFiles/MedicalFiles.tsx
@@ -1,43 +1,39 @@
-import { SetStateAction, useState } from 'react';
-import { Container, Row, Col, Form, Button, Table } from 'react-bootstrap';
-import { ToastContainer, toast } from 'react-toastify';
-import { PlusLg } from 'react-bootstrap-icons';
-import { useNavigate } from 'react-router-dom';
+import { SetStateAction, useState } from "react";
+import { Container, Row, Col, Form, Button, Table } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+import { PlusLg } from "react-bootstrap-icons";
+import { useNavigate } from "react-router-dom";
+import MedicalFilesData from "./MedicalFilesData";
 
 const MedicalFiles = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   const navigate = useNavigate();
 
   const [pets, setPets] = useState([
-    { id: 1, petName: 'Buddy', ownerName: 'Tony', breed: 'Golden Retriever' },
-    { id: 2, petName: 'Milo', ownerName: 'Sarah', breed: 'Beagle' },
-    { id: 3, petName: 'Max', ownerName: 'John', breed: 'Bulldog' },
-    { id: 4, petName: 'Bella', ownerName: 'Anna', breed: 'Poodle' },
+    { id: 1, petName: "Buddy", ownerName: "Tony", breed: "Golden Retriever" },
+    { id: 2, petName: "Milo", ownerName: "Sarah", breed: "Beagle" },
+    { id: 3, petName: "Max", ownerName: "John", breed: "Bulldog" },
+    { id: 4, petName: "Bella", ownerName: "Anna", breed: "Poodle" },
   ]);
 
-  const handleSearchChange = (e: { target: { value: SetStateAction<string>; }; }) => {
+  const handleSearchChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
     setSearchTerm(e.target.value);
   };
 
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     if (!searchTerm) {
-      toast.error('Search field is required', {
+      toast.error("Search field is required", {
         autoClose: 1500,
-        theme: 'colored',
+        theme: "colored",
       });
     }
   };
 
-  const handleAddPet = () => {
-    
-  };
-
-  const handleViewPetDetails = (id: number) => {
-    navigate(`MedicalFileInfo`);
-    //navigate(`petdetails/${id}`);
-  };
+  const handleAddPet = () => {};
 
   return (
     <div>
@@ -56,7 +52,12 @@ const MedicalFiles = () => {
                   />
                 </Col>
                 <Col xs="auto">
-                  <Button variant="primary" type="button" className='me-2' onClick={handleAddPet}>
+                  <Button
+                    variant="primary"
+                    type="button"
+                    className="me-2"
+                    onClick={handleAddPet}
+                  >
                     <PlusLg />
                   </Button>
                 </Col>
@@ -73,23 +74,7 @@ const MedicalFiles = () => {
                     <th className="text-center">Actions</th>
                   </tr>
                 </thead>
-                <tbody>
-                  {pets.map((pet, index) => (
-                    <tr key={index}>
-                      <td>{pet.id}</td>
-                      <td>{pet.petName}</td>
-                      <td>{pet.ownerName}</td>
-                      <td>{pet.breed}</td>
-                      <td className="text-center">
-                        <div className="d-flex justify-content-center">
-                          <Button variant="primary" size="sm" className="mr-2" style={{ width: '50%' }} onClick={() => handleViewPetDetails(pet.id)}>
-                            View Details
-                          </Button>
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
+                <tbody>{pets.map((pet) => MedicalFilesData(pet))}</tbody>
               </Table>
             </div>
           </Col>
