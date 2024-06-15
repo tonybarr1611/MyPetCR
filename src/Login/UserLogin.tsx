ace27fa4cc1d02ee82119ba333c0ad4170497d19
tony1611 <antbarrantes@estudiantec.cr>
2024-06-14 23:33:11 -0600

    Login name fix
    Fixed bug where the GitHub repo assigned a lowercase name which prevents remote deployment.

--- src/Login/UserLogin.tsx
+++ src/Login/UserLogin.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import { EyeFill, EyeSlashFill, PersonLock } from "react-bootstrap-icons";
-import "./Login.css";
+import "./UserLogin.css";
 
 //personfilladd - personfillcheck - search
 
