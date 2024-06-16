b5d9588a7cfa4ef8187f0e2c2ee92e65caead7c3
PavelCA04 <bzamora@estudiantec.cr>
2024-06-15 23:22:16 -0600

    Rerouting
    Rerouting new elements developed into Dashboard/App and Clientside

--- src/ClientSide/ClientSide.tsx
+++ src/ClientSide/ClientSide.tsx
@@ -1,5 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
+import AccessDenied_SignIn from "./Pet Management/AccessDenied_SignIn";
+import ClientOptions from "./Pet Management/ClientOptions";
+import ClientPets from "./Pet Management/ClientPets/ClientPets";
+import ClientAppointments from "./Pet Management/ClientAppointments/ClientAppointments";
+import RegisterPet from "./Pet Management/ClientPets/RegisterPet";
+import RequestAppointment from "./Pet Management/ClientAppointments/RequestAppointment";
+import Profile from "./Profile";
 
 const setBackgroundWhite = () => {
   var html = document.getElementsByTagName("html");
@@ -17,7 +24,13 @@ function ClientSide() {
       <Routes>
         <Route path="/" />
         {/* <Route path="/shop" element={<Shop />} /> */}
-        {/* <Route path="/management" element={<Management />} /> */}
+        <Route path="/accessdeniedsignin" element={<AccessDenied_SignIn />} />
+        <Route path="/management" element={<ClientOptions />} />
+        <Route path="/management/pets" element={<ClientPets />} />
+        <Route path="/management/appointments" element={<ClientAppointments />} />
+        <Route path="/management/pets/addpet" element={<RegisterPet />} />
+        <Route path="/management/appointments/requestappointment" element={<RequestAppointment />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
