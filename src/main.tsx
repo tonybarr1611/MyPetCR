53ce226ab389352d1944e1dc83180f87648925f7
tony1611 <antbarrantes@estudiantec.cr>
2024-06-16 15:02:31 -0600

    Shop connected to backend
    Retrieved data, using the backend, and displayed it on the shop

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const backendURL = "http://localhost:8000/api/v1/";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+export { backendURL };
