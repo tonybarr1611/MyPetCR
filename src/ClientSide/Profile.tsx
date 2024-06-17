272b60ebacc72a3056e92a30984bdffc33bd1aa3
tony1611 <antbarrantes@estudiantec.cr>
2024-06-17 12:44:55 -0600

    Profile display
    Displays valid data in the profile page

--- src/ClientSide/Profile.tsx
+++ src/ClientSide/Profile.tsx
@@ -1,19 +1,24 @@
-import { Container, Row, Col, Card } from 'react-bootstrap';
-import { FaCircleUser } from 'react-icons/fa6';
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { FaCircleUser } from "react-icons/fa6";
+import { getProfileData } from "./Functions";
 
 const Profile = () => {
+  const [user, setUser] = useState({ name: "", email: "", phone: "" });
 
-  const user = {
-    name: "John Doe",
-    email: "john.doe@example.com",
-    phone: "(123) 456-7890"
-  };
+  useEffect(() => {
+    async function fetchUser() {
+      const user = await getProfileData();
+      setUser(user);
+    }
+    fetchUser();
+  }, []);
 
   return (
     <Container className="mt-5">
       <Row className="d-flex justify-content-center">
         <Col md={6}>
-          <Card style={{background: "#C9E5F0"}}>
+          <Card style={{ background: "#C9E5F0" }}>
             <Card.Body className="text-center">
               <Card.Title className="mb-4">Profile</Card.Title>
               <Card.Text>
@@ -26,8 +31,12 @@ const Profile = () => {
                 <strong>Phone:</strong> {user.phone}
               </Card.Text>
               <FaCircleUser size={40} className="text-primary" />
-              <Card.Text className="mt-4" style={{ fontSize: "small", color: "grey" }}>
-                If you need to change your information, please contact one of our clinics through message, call, or in person.
+              <Card.Text
+                className="mt-4"
+                style={{ fontSize: "small", color: "grey" }}
+              >
+                If you need to change your information, please contact one of
+                our clinics through message, call, or in person.
               </Card.Text>
             </Card.Body>
           </Card>
