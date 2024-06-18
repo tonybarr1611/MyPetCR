7873aeb575173989e412bf58935c1d1a1f3e8ca4
Victor Aymerich Quesada <vaymerich@estudiantec.cr>
2024-06-18 01:12:36 -0600

    Fix to update pet

--- backend/controllers/pet.ts
+++ backend/controllers/pet.ts
@@ -75,7 +75,7 @@ async function UpdatePet(req: Request, res: Response) {
 
     const IDBreed = req.body.IDBreed || pet?.recordset[0].IDBreed;
     const IDClient = req.body.IDClient || pet?.recordset[0].IDClient;
-    const Name = req.body.Name || pet?.recordset[0].Name;
+    const Name = req.body.Name || pet?.recordset[0].PetName;
     const Birthdate = req.body.Birthdate || pet?.recordset[0].Birthdate;
     const Weight = req.body.Weight || pet?.recordset[0].Weight;   
     const Notes = req.body.Notes || pet?.recordset[0].Notes;    
@@ -91,7 +91,7 @@ async function UpdatePet(req: Request, res: Response) {
         [{ name: 'IDClient', type: sql.Int, value: IDClient }]
     );
     if (client?.recordset.length == 0) { return res.status(404).send("Pet not updated: Client not found"); }
-
+    
     await executeProcedure(res,
         'UpdatePet',
         [
@@ -99,7 +99,7 @@ async function UpdatePet(req: Request, res: Response) {
             { name: 'IDBreed', type: sql.Int, value: IDBreed },
             { name: 'IDClient', type: sql.Int, value: IDClient },
             { name: 'Name', type: sql.NVarChar(255), value: Name },
-            { name: 'Birthdate', type: sql.NVarChar(255), value: Birthdate },
+            { name: 'Birthdate', type: sql.DateTime , value: Birthdate },
             { name: 'Weight', type: sql.Int, value: Weight },
             { name: 'Notes', type: sql.NVarChar(512), value: Notes }
         ],
