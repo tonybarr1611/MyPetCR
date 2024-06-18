709dcec3ee67366f5720b79a098dcc5a9b5323ca
PavelCA04 <bzamora@estudiantec.cr>
2024-06-18 03:32:06 -0600

    Register Pets implemented in Backend
    Register pets implemented with backend, can connect and add pets

--- src/ClientSide/Pet Management/ClientPets/RegisterPet.tsx
+++ src/ClientSide/Pet Management/ClientPets/RegisterPet.tsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
-import { Container, Form, Button, Card } from "react-bootstrap";
-import { ToastContainer, toast } from "react-toastify";
+import { useState, useEffect } from "react";
+import { Container, Card, Form, Button, Table } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { MdPets } from "react-icons/md";
+import { ToastContainer, toast } from "react-toastify";
 import { getClientID } from "../../Functions";
 import axios from "axios";
+
+const petTypeMapping = {
+  1: "Dog",
+  2: "Cat",
+  3: "Bird"
+};
 
 const RegisterPet = () => {
   const navigate = useNavigate();
@@ -17,7 +23,30 @@ const RegisterPet = () => {
     notes: "",
   });
 
-  const handleOnChange = (e) => {
+  const [breeds, setBreeds] = useState<{ IDBreed: number; BreedName: string; }[]>([]);
+  const [showBreedTable, setShowBreedTable] = useState(false);
+
+  useEffect(() => {
+    const fetchBreeds = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/api/v1/breed");
+        const breeds = response.data.map((obj:any) => ({
+          IDBreed: obj.IDBreed,
+          BreedName: obj.Name,
+        }));
+        setBreeds(breeds);
+      } catch (error) {
+        console.error("Error fetching breeds:", error);
+        toast.error("Error fetching breeds", {
+          autoClose: 1500,
+          theme: "colored",
+        });
+      }
+    };
+    fetchBreeds();
+  }, []);
+
+  const handleOnChange = (e: { target: { name: any; value: any; }; }) => {
     setPetDetails({ ...petDetails, [e.target.name]: e.target.value });
   };
 
@@ -25,7 +54,9 @@ const RegisterPet = () => {
     navigate("/clientside/management/pets");
   };
 
-  const handleSubmit = async (e) => {
+  const clientID = getClientID();
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
     let weightRegex = /^[0-9]+$/;
 
@@ -51,32 +82,38 @@ const RegisterPet = () => {
           autoClose: 1500,
           theme: "colored",
         });
-      }else if (petDetails.notes.length > 512) {
+      } else if (petDetails.notes.length > 512) {
         toast.error("Notes should be less than 512 characters", {
           autoClose: 1500,
           theme: "colored",
         });
       } else {
-        const postData = {
-          IDBreed: parseInt(petDetails.breed),
-          IDClient: getClientID(), // Replace with actual IDClient logic
-          Name: petDetails.name,
-          Birthdate: petDetails.birthdate,
-          Weight: parseInt(petDetails.weight),
-          Notes: petDetails.notes,
-        };
-        console.log("Post data: ", postData);
-        const response = await axios.post(
-          "http://localhost:8080/api/v1/pet",
-          postData
-        );
+        try {
+          const url = `http://localhost:8080/api/v1/pet`;
+          const params = {
+            IDBreed: petDetails.breed,
+            IDClient: `${clientID}`,
+            Name: petDetails.name,
+            Birthdate: petDetails.birthdate,
+           // IDPetType: petDetails.petType,
+            Weight: petDetails.weight,
+            Notes: petDetails.notes,
+          };
+          console.log(params);
+          await axios.post(url, params);
+          toast.success("Pet registered successfully!", {
+            autoClose: 1500,
+            theme: "colored",
+          });
 
-        toast.success("Pet registered successfully!", {
-          autoClose: 1500,
-          theme: "colored",
-        });
-
-        navigate("/clientside/management/pets");
+          navigate("/clientside/management/pets");
+        } catch (error) {
+          console.error("Error registering pet:", error);
+          toast.error("An error occurred registering pet. Please try again.", {
+            autoClose: 1500,
+            theme: "colored",
+          });
+        }
       }
     } catch (error) {
       console.error("Error registering pet:", error);
@@ -87,42 +124,54 @@ const RegisterPet = () => {
     }
   };
 
+  const toggleBreedTable = () => {
+    setShowBreedTable(!showBreedTable);
+  };
+
   return (
-    <Container
-      className="d-flex justify-content-center align-items-center"
-      style={{ minHeight: "100vh" }}
-    >
+    <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
       <ToastContainer position="top-center" />
       <Card style={{ width: "24rem", background: "#C9E5F0" }}>
         <Card.Body>
           <div className="text-center mb-4">
-            <MdPets
-              size={40}
-              className="mb-3"
-              style={{ color: "var(--darkblue)" }}
-            />
+            <MdPets size={40} className="mb-3" style={{ color: "var(--darkblue)" }} />
             <h1 className="h4">Register Pet</h1>
           </div>
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formPetType">
               <Form.Label>Pet Type</Form.Label>
               <Form.Control
-                type="text"
+                as="select"
                 name="petType"
                 value={petDetails.petType}
                 onChange={handleOnChange}
-                placeholder="Enter pet type (e.g., Dog)"
-              />
+              >
+                <option value="">Select Pet Type</option>
+                {Object.entries(petTypeMapping).map(([id, type]) => (
+                  <option key={id} value={id}>{type}</option>
+                ))}
+              </Form.Control>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBreed">
-              <Form.Label>Breed</Form.Label>
-              <Form.Control
-                type="text"
-                name="breed"
-                value={petDetails.breed}
-                onChange={handleOnChange}
-                placeholder="Enter breed"
-              />
+              <Form.Label>Breed ID</Form.Label>
+              <div className="d-flex">
+                <Form.Control
+                  type="number"
+                  name="breed"
+                  value={petDetails.breed}
+                  onChange={handleOnChange}
+                  placeholder="Enter breed ID"
+                  min="1"
+                />
+                <Button
+                  variant="info"
+                  onClick={toggleBreedTable}
+                  className="ml-2"
+                  style={{ width: "70%" }}
+                >
+                  {showBreedTable ? "Hide Breeds" : "Show Breeds"}
+                </Button>
+              </div>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formName">
               <Form.Label>Name</Form.Label>
@@ -165,16 +214,46 @@ const RegisterPet = () => {
               />
             </Form.Group>
             <div className="d-flex justify-content-between">
-              <Button variant="primary" type="submit" className="w-100 mb-3 mr-5">
+              <Button variant="primary" type="submit" className="mb-3">
                 Register Pet
               </Button>
-              <Button variant="secondary" type="button" className="w-100 mb-3" onClick={handleCancel}>
+              <Button variant="secondary" type="button" className="mb-3" onClick={handleCancel}>
                 Cancel
               </Button>
             </div>
           </Form>
         </Card.Body>
       </Card>
+      {showBreedTable && (
+        <div style={{ marginLeft: "20px" }}>
+          <div className="scrollableDiv datatable">
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Breed</th>
+                </tr>
+              </thead>
+              <tbody>
+                {breeds.map((breed, index) => (
+                  <tr
+                    key={index}
+                    onClick={() =>
+                      setPetDetails({
+                        ...petDetails,
+                        breed: breed.IDBreed.toString(),
+                      })
+                    }
+                  >
+                    <td>{breed.IDBreed}</td>
+                    <td>{breed.BreedName}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      )}
     </Container>
   );
 };
